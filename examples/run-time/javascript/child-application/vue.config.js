1be03289404de4db50f4796c9e795cc329751206
RyanLafferty <rlafferty1@hotmail.com>
2020-03-25 17:16:19 -0400

    Updated child application name and disabled source maps.

--- examples/run-time/javascript/child-application/vue.config.js
+++ examples/run-time/javascript/child-application/vue.config.js
@@ -2,6 +2,7 @@ const WebpackAssetsManifest = require('webpack-assets-manifest');
 
 // source: https://tarikhamilton.com/blog/2019/03/25/create-an-asset-manifest-json-like-create-react-app-using-vue-cli/
 module.exports = {
+  productionSourceMap: false,
   configureWebpack: config => {
     config.plugins = config.plugins.concat(
       new WebpackAssetsManifest({
